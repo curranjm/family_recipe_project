dbb9dab73a953d5ea34473d30f600d09383f8d69
Jason Curran <curran.jm@gmail.com>
2017-10-13 19:15:24 -0400

    added use of instance folder

--- project/__init__.py
+++ project/__init__.py
@@ -6,6 +6,12 @@ from flask import Flask
 ################
 #### config ####
 ################
+"""
+The ‘instance_relative_config’ flag being set to true causes the loading of
+configuration files to be relative to the instance folder instead of the default
+setting of the root (top-level) directory.
+"""
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
 
-app = Flask(__name__)
 from . import views
