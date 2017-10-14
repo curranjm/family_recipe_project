c0d46570ed6ae313187a2a200d0762faec02cb32
Jason Curran <curran.jm@gmail.com>
2017-10-13 20:10:31 -0400

    Added use of blueprints

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,11 @@
-#################
-#### imports ####
-#################
+################################################################################
+# imports
+################################################################################
 from flask import Flask
 
-################
-#### config ####
-################
+################################################################################
+# config
+################################################################################
 """
 The ‘instance_relative_config’ flag being set to true causes the loading of
 configuration files to be relative to the instance folder instead of the default
@@ -14,4 +14,13 @@ setting of the root (top-level) directory.
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
 
-from . import views
+
+################################################################################
+# blueprints
+################################################################################
+from project.users.views import users_blueprint
+from project.recipes.views import recipes_blueprint
+
+# register the blueprints
+app.register_blueprint(users_blueprint)
+app.register_blueprint(recipes_blueprint)
---
+++ project/tests/test_users.py
@@ -0,0 +1,33 @@
+import unittest
+
+from project import app
+
+class ProjectTests(unittest.TestCase):
+    ############################################################################
+    # Setup / Teardown
+    ############################################################################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+        self.assertEquals(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    ############################################################################
+    # Tests
+    ############################################################################
+
+    def test_main_page(self):
+        response = self.app.get('/login', follow_redirects=True)
+        self.assertIn(b'Future site for logging into Family Recipes!', response.data)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
